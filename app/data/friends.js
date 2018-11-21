5a23257df99a401db025f848d674475bba67db0b
Max Wilets <mwilets@gmail.com>
2018-11-21 13:50:44 -0700

    friend data

---
+++ app/data/friends.js
@@ -0,0 +1,105 @@
+var people = [{
+        name: 'Taylor',
+        picture: 'images/taylore.jpg',
+        looking: 1,
+        scores: [
+            2, 3, 5, 4, 5, 2, 1, 4, 3, 2
+        ]
+    },
+    {
+        name: 'Doug',
+        picture: 'images/doug.png',
+        looking: 2,
+        scores: [
+            3, 1, 4, 5, 3, 5, 4, 5, 2, 5
+        ]
+    },
+    {
+        name: 'Daria',
+        picture: 'images/daria.jpg',
+        looking: 1,
+        scores: [
+            1, 5, 3, 3, 2, 3, 5, 3, 2, 1
+        ]
+    },
+    {
+        name: "Camron",
+        picture: 'images/cameron.jpeg',
+        looking: 4,
+        scores: [
+            4, 2, 4, 5, 3, 4, 2, 5, 3, 2
+        ]
+    },
+    {
+        name: 'Titus',
+        picture: 'images/titus.jpg',
+        looking: 3,
+        scores: [
+            1, 2, 2, 5, 1, 5, 3, 4, 3, 4
+        ]
+    },
+    {
+        name: 'Juice',
+        picture: 'images/juice.jpg',
+        looking: 2,
+        scores: [
+            2, 3, 3, 4, 5, 4, 3, 1, 1, 3
+        ]
+    },
+    {
+        name: 'Posty',
+        picture: 'images/post.jpg',
+        looking: 2,
+        scores: [
+            5, 3, 4, 5, 2, 3, 5, 1, 4, 2
+        ]
+    },
+    {
+        name: 'Kimmy',
+        picture: 'images/kimmy.jpg',
+        looking: 1,
+        scores: [
+            5, 4, 1, 3, 5, 5, 2, 5, 4, 1
+        ]
+    },
+    {
+        name: 'Nora',
+        picture: 'images/nora.jpg',
+        looking: 1,
+        scores: [
+            5, 4, 1, 2, 1, 3, 2, 5, 4, 3
+        ]
+    },
+    {
+        name: 'Chad',
+        picture: 'images/chad.jpg',
+        looking: 3,
+        scores: [
+            5, 3, 4, 2, 4, 5, 3, 1, 3, 3
+        ]
+    },
+    {
+        name: 'Sally',
+        picture: 'images/sally.jpg',
+        looking: 4,
+        scores: [
+            3, 5, 3, 1, 2, 5, 3, 1, 2, 1
+        ]
+    },
+    {
+        name: 'Tobias',
+        picture: 'images/tobias.jpg',
+        looking: 3,
+        scores: [
+            2, 1, 4, 3, 4, 2, 5, 3, 2, 5
+        ]
+    },
+    {
+        name: 'Jenny',
+        picture: 'images/jenny.jpg',
+        looking: 1,
+        scores: [
+            3, 2, 5, 3, 4, 1, 5, 3, 2, 1
+        ]
+    }
+]
